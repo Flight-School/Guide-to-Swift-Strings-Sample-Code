1cb15bdd70ceb0c20a0036d66c49ad6f3134d83c
Mattt <mattt@me.com>
2019-02-14 13:23:46 -0800

    Formatting

--- Chapter 5/HumanReadable.playground/Contents.swift
+++ Chapter 5/HumanReadable.playground/Contents.swift
@@ -1,8 +1,6 @@
 import Foundation
 
-let data = Data(bytes: [0xB2, 0x03,
-                        0xE2, 0x8F,
-                        0xA5, 0x25,
-                        0xBE, 0x47])
+let data = Data(bytes: [0xB2, 0x03, 0xE2, 0x8F,
+                        0xA5, 0x25, 0xBE, 0x47])
 
 data.humanReadableEncodedString()
