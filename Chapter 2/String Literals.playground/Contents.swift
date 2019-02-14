c4e9964da3475e33abbeef2dce1538aac5383af6
Mattt <mattt@me.com>
2019-02-14 12:26:17 -0800

    Add more examples of string literals

--- Chapter 2/String Literals.playground/Contents.swift
+++ Chapter 2/String Literals.playground/Contents.swift
@@ -14,4 +14,20 @@ and your tray table is stowed.
 
 let stringWithInterpolation = "7 + 4 + 7 = \(7 + 4 + 7)"
 
-let rawString = #"\(7 + 4 + 7)"#
+let singleLineRawString = #"\(7 + 4 + 7)"#
+
+let multilineRawString = #"""
+ \-----------------------\
+  \                       \
+   \      ___              \
+    \    (_  /'_ /_/        \        __
+     \   /  (/(//)/          \       | \
+      >      _/               >------|  \       ______
+     /     __                /       --- \_____/**|_|_\____  |
+    /     (  _ /     /      /          \_______ --------- __>-}
+   /     __)( /)()()(      /              /  \_____|_____/   |
+  /                       /               *         |
+ /-----------------------/                         {o}
+"""#
+
+let rawStringWithInterpolation = #"\#(7 + 4 + 7)"#
